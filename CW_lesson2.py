e807f623126a070bf678376976ae64963669f4a8
Андрій <andrii.lishchenko1@gmail.com>
2025-09-29 10:18:02 +0300

    Add files via upload

---
+++ CW_lesson2.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+# image = cv2.imread("images/img11.jpg")
+# #image = cv2.resize(image, (1700,900))
+#
+# image = cv2.resize(image, (image.shape[1] // 2, image.shape[0] // 2))
+# print(image.shape)
+# #image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+# #image = cv2.flip(image, 0)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image = cv2.Canny(image, 100, 100)
+# #cv2.imshow("image", image[200:400, 400:600])#ОБРІЗКА
+# #image = cv2.GaussianBlur(image, (7, 7), 3)#рівень блюру тільки непарнічисла
+# #1 жирність контура
+# #image = cv2.dilate(image, None, iterations=1)
+# kernel = np.ones((5, 5), np.uint8)
+# image = cv2.dilate(image, kernel, iterations = 1)
+# image = cv2.erode(image, kernel, iterations = 1)
+# cv2.imshow('weat', image)
+video_capture = cv2.VideoCapture(0)
+while True:
+    miss, frame = video_capture.read()
+    frame = cv2.resize(frame, (800, 600))
+    cv2.imshow("video", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
